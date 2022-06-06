7311ca4efa077ddc3690bd092b126e6b4c0ccd26
Juliana Costa <julianacosta_s@hotmail.com>
2022-06-06 20:54:31 +1000

    Challenge completed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //IBOutlet when we want to change something in the interface
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-    
+  
+    //Action to the button "Roll" When an intercation with the user interface leads to something int the code
     @IBAction func rollDice(_ sender: UIButton) {
         
-        diceImageView1.image = diceArray[Int.random(in: 1...5)]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [Int.random(in: 1...5)]
+        //All elements are displayed in the array
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        //Two different ways to randomise elements in an array
+        diceImageView1.image = diceArray.randomElement() //Call random.Element() to randomise from all elemnts in the array or
+        diceImageView2.image = diceArray[Int.random(in: 0...5)] //Specify the range of elements you want to randomise
        
     }
 }
